e472ce6ba6ad5d24b566a0d70afc111454948ef8
octavio masdeu <masdeuocta@gmail.com>
2021-10-13 23:20:12 -0300

    mas juegos en el array

--- array.js
+++ array.js
@@ -11,13 +11,22 @@ class Juego {
 }
 
 const juegos = [
-    new Juego ("Dark Souls", 1500, "accion", 01, "./geimer/juego01.jpg"),
+    new Juego ("Dark Souls", 1500, "accion, Exploracion", 01, "./geimer/juego01.jpg"),
     new Juego ("Elden Ring", 3200, "accion, aventura", 02,"./geimer/juego02.jpg" ),
     new Juego ("Sekiro", 2500,"Accion", 03,"./geimer/juego03.jpg" ),
     new Juego ("God of War", 4000,"Accion, aventura", 04, "./geimer/juego04.jpg" ) ,
-    new Juego ("Shadow od the collosus", 1200,"Estrategia, Exploracion", 05, "./geimer/juego05.jpg"),
+    new Juego ("Shadow of the collosus", 1200,"Estrategia, Exploracion", 05, "./geimer/juego05.jpg"),
     new Juego ("Resident Evil 2", 1200,"Survival Horror", 06,"./geimer/juego06.jpg" ),
     new Juego ("Resident Evil: Village", 2900,"Survival Horror, Accion", 07, "./geimer/juego07.jpg"),
+    new Juego ("Bloodborne", 3500, "Souls, Accion, Exploracion", 08, ""),
+    new Juego ("Demos Souls", 5900, "Accion, Exploracion", 09,""),
+    new Juego ("Dark souls 2", 800, "Accion, Souls", 10, ""),
+    new Juego ("Dark souls 3", 1200, "Accion, Souls", 11, ""),
+    new Juego ("Hollow Knight", 300, "Metroidvania", 12, ""),
+    new Juego ("Silent Hills : Homecoming", 699, "Survival Horror, Terror Psicologico ", 13, ""),
+    new Juego ("Red Dead Redemption", 1200, "Accion, Aventura, Mundo Abierto", 14, ""),
+    new Juego ("Back 4 Blood", 3400, "Zombies, FPS, Cooperativo", 15, ""),
+    new Juego ("CupHead", 500, "SoulsLike, 2D, Plataformas", 16, ""),
 ]
 
 //carrito
